52d557a9f2c465d13a8d8a33d2d346d588b93e64
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-27 16:06:33 +0300

    Driving values, outputting questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [questionIndex, setQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>The current question </p>;
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswer) => [...prevAnswer, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div className="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
