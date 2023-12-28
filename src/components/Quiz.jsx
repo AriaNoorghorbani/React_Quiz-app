2e3969507918565779c4f6ba0e66b7df475cf55f
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-28 08:29:33 +0300

    shuffle answer and complete the quiz circle

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import completeImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
+  const questionIsComplete = activeQuestionIndex === QUESTIONS.length;
+
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswer) => [...prevAnswer, selectedAnswer]);
   }
+
+  if (questionIsComplete) {
+    return (
+      <div id="summary">
+        <img src={completeImg} alt="Complete Image" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const answerShuffled = QUESTIONS[activeQuestionIndex].answers;
+  answerShuffled.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div className="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {answerShuffled.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
